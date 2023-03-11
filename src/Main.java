0998c1a04fcf1bf5c67b2c81bcd25af50c2d0699
Ars_tosha <arsentev98pochta@mail.ru>
2023-03-11 15:53:10 +0500

    ДЗ #2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         Book warAndPeace = new Book(new Author("Лев Николаевич","Толстой"),"Война и мир",1867);
-        System.out.println(warAndPeace.getAuthor().getName() + " " + warAndPeace.getAuthor().getSurName());
-        System.out.println(warAndPeace.getNameBook());
-        System.out.println(warAndPeace.getYearBook());
+        System.out.println(new Book(new Author("Лев Николаевич" ,"Толстой"), "Война и мир", 1867));
 
         System.out.println();
-
         Book crimeAndPunishment = new Book(new Author("Федор Михайлович","Достоевский"),"Преступление и наказание",1866);
-        System.out.println(crimeAndPunishment.getAuthor().getName() + " " + crimeAndPunishment.getAuthor().getSurName());
-        System.out.println(crimeAndPunishment.getNameBook());
-        System.out.println(crimeAndPunishment.getYearBook());
+        System.out.println(new Book(new Author("Федор Михайлович" ,"Достоевский"), "Преступление и наказание", 1866));
 
+        System.out.println();
         crimeAndPunishment.setYearBook(1865);
-        System.out.println(crimeAndPunishment.getYearBook());
+        System.out.println("Преступление и наказание " + crimeAndPunishment.getYearBook());
+        
+        System.out.println();
+        System.out.println(warAndPeace.equals(crimeAndPunishment));
+
     }
 }
